29a5796529475abaf2339449744038ad75d3d552
Bruno Amorim <bruno.amorim032@gmail.com>
2021-07-17 16:53:50 -0300

    mail form start

--- script.js
+++ script.js
@@ -1,3 +1,17 @@
 function footerUpdate() {
     document.write(new Date().getFullYear())
 }
+
+function sendEmail(name, email, message) {
+    Email.send({
+        Host : "smtp.yourisp.com",
+        Username : "username",
+        Password : "password",
+        To : 'bruno.amorim032@gmail.com',
+        From : "you@isp.com",
+        Subject : `${name} te enviou uma mensagem pelo portfolio.`,
+        Body : `Name: ${name} <br/> Email: ${email} <br/> Message: ${message}`
+    }).then(
+      () => alert('E-mail enviado com sucesso!')
+    );
+}
