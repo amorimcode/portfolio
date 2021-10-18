cbac2b5055e90b7075dacd9d43372b7a1b14ce9e
Amorim da Silva <amorim@energiaautomacao.com.br>
2021-10-18 08:20:41 -0300

    color fix

--- script.js
+++ script.js
@@ -1,22 +1,27 @@
-function sendEmail() {
-    
-    const name = document.getElementById('name').value
-    const email = document.getElementById('email').value
-    const subject = document.getElementById('subject').value
-    const message = document.getElementById('message').value
-    
-    const button = document.getElementById('submitButton')
+// function sendEmail() {
 
-    Email.send({
-        Host : "smtp.gmail.com",
-        Username : "bruno.emailjs@gmail.com",
-        Password : "clacle01",
-        To : 'bruno.amorim032@gmail.com',
-        From : "bruno.emailjs@gmail.com",
-        Subject : `${name}: ${subject}`,
-        Body : `Name: ${name} <br/> Email: ${email} <br/> Message: ${message}`
-    }).then(
-        () => alert('E-mail enviado com sucesso!'),
-        button.disabled = true
-    );
-}
+//     const name = document.getElementById('name').value
+//     const email = document.getElementById('email').value
+//     const subject = document.getElementById('subject').value
+//     const message = document.getElementById('message').value
+
+//     const button = document.getElementById('submitButton')
+
+//     Email.send({
+//         Host : "smtp.gmail.com",
+//         Username : "bruno.emailjs@gmail.com",
+//         Password : "clacle01",
+//         To : 'bruno.amorim032@gmail.com',
+//         From : "bruno.emailjs@gmail.com",
+//         Subject : `${name}: ${subject}`,
+//         Body : `Name: ${name} <br/> Email: ${email} <br/> Message: ${message}`
+//     }).then(
+//         () => alert('E-mail enviado com sucesso!'),
+//         button.disabled = true
+//     );
+// }
+
+
+const sum = (a, b) => {return a + b}
+
+sum(1, 2)
